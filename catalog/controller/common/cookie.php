2176ad9111c2072463feaf2fe10ae5cb8a8fef38
Yehor <egorkorsungit@gmail.com>
2025-08-10 15:41:21 +0200

    всплывашка про файлы куки добавлена

--- catalog/controller/common/cookie.php
+++ catalog/controller/common/cookie.php
@@ -1,18 +1,17 @@
 <?php
 class ControllerCommonCookie extends Controller {
     public function index() {
-        if (isset($this->session->data['cookie_consent'])) {
+        // скрываем ТОЛЬКО если уже согласились
+        if (!empty($this->session->data['cookie_consent']) && $this->session->data['cookie_consent'] === true) {
             return '';
         }
 
         $this->load->language('common/cookie');
-
-        $data['text_cookie'] = $this->language->get('text_cookie');
-        $data['button_agree'] = $this->language->get('button_agree');
+        $data['text_cookie']     = $this->language->get('text_cookie');
+        $data['button_agree']    = $this->language->get('button_agree');
         $data['button_disagree'] = $this->language->get('button_disagree');
-        $data['agree'] = $this->url->link('common/cookie/agree');
+        $data['agree']    = $this->url->link('common/cookie/agree');
         $data['disagree'] = $this->url->link('common/cookie/disagree');
-
         return $this->load->view('common/cookie', $data);
     }
 
@@ -22,7 +21,8 @@ class ControllerCommonCookie extends Controller {
     }
 
     public function disagree() {
-        $this->session->data['cookie_consent'] = false;
+        // чтобы баннер снова показывался при «Отклонить»
+        unset($this->session->data['cookie_consent']);
         $this->response->setOutput('1');
     }
 }
